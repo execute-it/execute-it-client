bafce995501c7e911ef02f3ae691a4bee633be2d
pratikdaigavane <daigavanep@gmail.com>
2020-09-03 10:32:46 +0530

    dark mode state stored in local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import {Route, Switch,} from "react-router-dom";
-import { useThemeSwitcher } from "react-css-theme-switcher";
+import {useThemeSwitcher} from "react-css-theme-switcher";
 import SignIn from "./pages/sign-in/signIn.page"
 import './App.less'
 import DarkModeToggle from "react-dark-mode-toggle";
-import { Layout,Space} from "antd";
-const { Header} = Layout;
+import {Space} from "antd";
+import {reactLocalStorage} from 'reactjs-localstorage';
 
-const App = () =>{
-    const [isDarkMode, setIsDarkMode] = React.useState();
-    const { switcher, currentTheme, status, themes } = useThemeSwitcher();
+
+const App = () => {
+    const {switcher, status, themes} = useThemeSwitcher();
+
+    const [isDarkMode, setIsDarkMode] = React.useState( async ()=>{
+         const dark = await reactLocalStorage.get('theme', 'light') !== 'light';
+        await switcher({theme: dark ? themes.dark : themes.light});
+        return dark
+    })
+
     const toggleTheme = (isChecked) => {
         setIsDarkMode(isChecked);
-        switcher({ theme: isChecked ? themes.dark : themes.light });
+        reactLocalStorage.set('theme',isChecked?'dark':'light')
+        switcher({theme: isChecked ? themes.dark : themes.light});
     };
 
     // Avoid theme change flicker
@@ -20,24 +28,25 @@ const App = () =>{
         return null;
     }
     return (
-    <>
+        <>
 
-        <Space style={{ float: "right", padding: "10px 30px"}}>
-            <DarkModeToggle
-                onChange={toggleTheme}
-                checked={isDarkMode}
-                size={80}
-            />
-        </Space>
+            <Space style={{float: "right", padding: "10px 30px"}}>
+                <DarkModeToggle
+                    onChange={toggleTheme}
+                    checked={isDarkMode}
+                    size={80}
+                />
+            </Space>
 
 
-    <Switch>
-        <Route path='/login'>
-            <SignIn/>
-        </Route>
-    </Switch>
+            <Switch>
+                <Route path='/login'>
+                    <SignIn/>
+                </Route>
+            </Switch>
 
-    </>
-);}
+        </>
+    );
+}
 
 export default App;
