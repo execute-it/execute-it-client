1ae77b59aa772fbc61a35d1ed9ffa1a9ddf9fe39
pratikdaigavane <daigavanep@gmail.com>
2020-09-07 08:24:41 +0530

    logout fixed

--- src/components/headerComponent/headerComponent.component.jsx
+++ src/components/headerComponent/headerComponent.component.jsx
@@ -8,7 +8,7 @@ const {Title} = Typography
 const menu = (
   <Menu>
     <Menu.Item>
-      <a target="_blank" rel="noopener noreferrer" href="/login">
+      <a href="/login">
         Logout
       </a>
     </Menu.Item>
