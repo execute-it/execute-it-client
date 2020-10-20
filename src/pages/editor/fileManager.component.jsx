919331022b5abaf75868992b1ec76944a4d2c1c2
AVC0706 <atharvvijay4@gmail.com>
2020-10-20 08:46:17 +0530

    Participant scroll and minor bugs

--- src/pages/editor/fileManager.component.jsx
+++ src/pages/editor/fileManager.component.jsx
@@ -1,246 +1,258 @@
 import React from "react";
 import GlobalContext from "../../context/GlobalContext";
-import {isNodeFolder} from "../../utils/utils";
-import {Button, Col, Input, message, Modal, Popconfirm, Row, Tree,} from "antd";
+import { isNodeFolder } from "../../utils/utils";
 import {
-    CaretRightOutlined,
-    DeleteOutlined,
-    FileAddOutlined,
-    FolderAddOutlined,
-    QuestionCircleOutlined,
+  Button,
+  Col,
+  Input,
+  message,
+  Modal,
+  Popconfirm,
+  Row,
+  Tree,
+} from "antd";
+import {
+  CaretRightOutlined,
+  DeleteOutlined,
+  FileAddOutlined,
+  FolderAddOutlined,
+  QuestionCircleOutlined,
 } from "@ant-design/icons";
 
-const {DirectoryTree} = Tree;
+const { DirectoryTree } = Tree;
 
 export default class FileManagerComponent extends React.Component {
-    static contextType = GlobalContext;
+  static contextType = GlobalContext;
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            treeNodes: null,
-            treeState: null,
-            isLoading: true,
-            newName: "",
-            visibleNewFileModal: false,
-            visibleNewFolderModal: false,
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      treeNodes: null,
+      treeState: null,
+      isLoading: true,
+      newName: "",
+      visibleNewFileModal: false,
+      visibleNewFolderModal: false,
+    };
+  }
+
+  componentDidMount() {
+    this.context.setInitStates(this.props.rtModel);
+    if (this.context.rtModel) {
+      this.setState({
+        treeNodes: this.props.rtModel.elementAt(["tree", "nodes"]),
+        treeState: this.context.getTreeState(),
+      });
+    }
+  }
+
+  componentWillMount() {
+    this.setState({ isLoading: false });
+  }
+
+  handleNewFile = () => {
+    // console.log(this.context.selectedId);
+    this.handleCancel();
+    this.context.addNewNode(
+      "file",
+      this.context.selectedId,
+      this.state.newName
+    );
+    this.setState({ newName: "" });
+  };
+
+  handleNewFolder = () => {
+    this.handleCancel();
+    this.context.addNewNode(
+      "folder",
+      this.context.selectedId,
+      this.state.newName
+    );
+    this.setState({ newName: "" });
+  };
+
+  handleDelete = () => {
+    const id = this.context.selectedId;
+    // console.log(this.props.rtModel.elementAt(["tree", "nodes"]));
+    if (isNodeFolder(this.props.rtModel.elementAt(["tree", "nodes"]), id)) {
+      // TODO: delete & remove tab recursively
+      this.context.deleteNode(id);
+    } else {
+      this.context.deleteNode(id);
+      this.context.tabRemove(id);
+      message.success("File Deleted !!");
+    }
+  };
+
+  getFileTreeObject = (root = null, rootId) => {
+    const nodes = this.props.rtModel.elementAt(["tree", "nodes"]); // == treeNodes of convergence code
+    if (!root) {
+      root = nodes.get("root");
+      rootId = "root";
     }
 
-    componentDidMount() {
-        this.context.setInitStates(this.props.rtModel);
-        if (this.context.rtModel) {
-            this.setState({
-                treeNodes: this.props.rtModel.elementAt(["tree", "nodes"]),
-                treeState: this.context.getTreeState(),
-            });
-        }
-    }
+    const childrenObj = root.get("children");
 
-    componentWillMount() {
-        this.setState({isLoading: false});
-    }
+    let children = [];
 
-    handleNewFile = () => {
-        // console.log(this.context.selectedId);
-        this.handleCancel();
-        this.context.addNewNode(
-            "file",
-            this.context.selectedId,
-            this.state.newName
-        );
-        this.setState({newName: ""});
-    };
-
-    handleNewFolder = () => {
-        this.handleCancel();
-        this.context.addNewNode(
-            "folder",
-            this.context.selectedId,
-            this.state.newName
-        );
-        this.setState({newName: ""});
-    };
-
-    handleDelete = () => {
-        const id = this.context.selectedId;
-        // console.log(this.props.rtModel.elementAt(["tree", "nodes"]));
-        if (isNodeFolder(this.props.rtModel.elementAt(["tree", "nodes"]), id)) {
-            // TODO: delete & remove tab recursively
-            this.context.deleteNode(id);
-        } else {
-            this.context.deleteNode(id);
-            this.context.tabRemove(id);
-            message.success("File Deleted !!");
-        }
-    };
-
-    getFileTreeObject = (root = null, rootId) => {
-        const nodes = this.props.rtModel.elementAt(["tree", "nodes"]); // == treeNodes of convergence code
-        if (!root) {
-            root = nodes.get("root");
-            rootId = "root";
-        }
-
-        const childrenObj = root.get("children");
-
-        let children = [];
-
-        childrenObj.forEach((child) => {
-            const id = child.value();
-            let node = nodes.get(id);
-            if (node.hasKey("children"))
-                children.push(this.getFileTreeObject(node, id));
-            else
-                children.push({
-                    title: node.get("name").value(), // Convert to string
-                    key: id, // Convert to string
-                    isLeaf: true,
-                });
+    childrenObj.forEach((child) => {
+      const id = child.value();
+      let node = nodes.get(id);
+      if (node.hasKey("children"))
+        children.push(this.getFileTreeObject(node, id));
+      else
+        children.push({
+          title: node.get("name").value(), // Convert to string
+          key: id, // Convert to string
+          isLeaf: true,
         });
+    });
 
-        // console.log(root.keys());
+    // console.log(root.keys());
 
-        let obj = {
-            title: root.get("name").value(),
-            key: rootId,
-            // Error prone Zone
-            children,
-        };
-
-        return obj;
+    let obj = {
+      title: root.get("name").value(),
+      key: rootId,
+      // Error prone Zone
+      children,
     };
 
-    onSelect = (keys, event) => {
-        // console.log("Trigger Select", keys, event);
-        // keys[0] reqd ID
+    return obj;
+  };
 
-        this.context.setSelectedId(keys[0]);
+  onSelect = (keys, event) => {
+    // console.log("Trigger Select", keys, event);
+    // keys[0] reqd ID
 
-        if (
-            !isNodeFolder(this.props.rtModel.elementAt(["tree", "nodes"]), keys[0])
-        ) {
-            this.context.openFile(keys[0]);
-        }
-    };
+    this.context.setSelectedId(keys[0]);
 
-    onExpand = () => {
-        // console.log("Trigger Expand");
-    };
+    if (
+      !isNodeFolder(this.props.rtModel.elementAt(["tree", "nodes"]), keys[0])
+    ) {
+      this.context.openFile(keys[0]);
+    }
+  };
 
-    handleCancel = () => {
-        // console.log("Clicked cancel button");
-        this.setState({
-            visibleNewFileModal: false,
-            visibleNewFolderModal: false,
-        });
-    };
+  onExpand = () => {
+    // console.log("Trigger Expand");
+  };
 
-    changeNewName = (e) => {
-        this.setState({newName: e.target.value});
-    };
+  handleCancel = () => {
+    // console.log("Clicked cancel button");
+    this.setState({
+      visibleNewFileModal: false,
+      visibleNewFolderModal: false,
+    });
+  };
 
-    render() {
-        const loading = this.state.isLoading;
-        const data = [];
-        data.push(this.getFileTreeObject());
+  changeNewName = (e) => {
+    this.setState({ newName: e.target.value });
+  };
 
-        // console.log(data)
-        // console.log("sID", this.context.selectedId)
+  render() {
+    const loading = this.state.isLoading;
+    const data = [];
+    data.push(this.getFileTreeObject());
 
-        return loading ? (
-            <div></div>
-        ) : (
-            <Row align="middle" style={{marginTop: "2rem"}}>
-                <Col align="middle" xs={24}>
-                    <Row gutter={[4, 12]}>
-                        <Col lg={24} xl={8}>
-                            <Button
-                                size="small"
-                                icon={<FileAddOutlined/>}
-                                type="primary"
-                                onClick={(_) => this.setState({visibleNewFileModal: true})}>
-                                File
-                            </Button>
-                        </Col>
-                        <Col lg={24} xl={8}>
+    // console.log(data)
+    // console.log("sID", this.context.selectedId)
+
+    return loading ? (
+      <div></div>
+    ) : (
+      <Row align="middle" style={{ marginTop: "2rem" }}>
+        <Col align="middle" xs={24}>
+          <Row gutter={[4, 12]}>
+            <Col lg={24} xl={8}>
+              <Button
+                size="small"
+                icon={<FileAddOutlined />}
+                type="primary"
+                onClick={(_) => this.setState({ visibleNewFileModal: true })}>
+                File
+              </Button>
+            </Col>
+            {/* <Col lg={24} xl={8}>
                             <Button
                                 size="small"
                                 icon={<FolderAddOutlined/>}
                                 type="primary"
                                 onClick={_ => this.setState({visibleNewFolderModal: true})}> Folder </Button>
-                        </Col>
-                        <Col lg={24} xl={8}>
-                            <Popconfirm
-                                disabled={
-                                    !this.context.selectedId ||
-                                    this.context.selectedId === "temproom"
-                                }
-                                placement="top"
-                                title="Are you sure？"
-                                icon={<QuestionCircleOutlined style={{color: "red"}}/>}
-                                onConfirm={this.handleDelete}>
-                                <Button
-                                    size="small"
-                                    icon={<DeleteOutlined/>}
-                                    type="primary"
-                                    danger
-                                    disabled={
-                                        !this.context.selectedId ||
-                                        isNodeFolder(
-                                            this.props.rtModel.elementAt(["tree", "nodes"]),
-                                            this.context.selectedId
-                                        )
-                                    }>
-                                    {" "}
-                                    Delete{" "}
-                                </Button>
-                            </Popconfirm>
-                        </Col>
-                        <Col lg={24} xl={8}>
-                            <Button
-                                size="small"
-                                icon={<CaretRightOutlined/>}
-                                type="primary"
-                                disabled={
-                                    !this.context.activeKey ||
-                                    isNodeFolder(
-                                        this.props.rtModel.elementAt(["tree", "nodes"]),
-                                        this.context.selectedId
-                                    )
-                                }
-                                onClick={this.context.runTerminal}>
-                                Run{" "}
-                            </Button>
-                        </Col>
-                    </Row>
-                </Col>
+                        </Col> */}
+            <Col lg={24} xl={8}>
+              <Popconfirm
+                disabled={
+                  !this.context.selectedId ||
+                  isNodeFolder(
+                    this.props.rtModel.elementAt(["tree", "nodes"]),
+                    this.context.selectedId
+                  )
+                }
+                placement="top"
+                title="Are you sure？"
+                icon={<QuestionCircleOutlined style={{ color: "red" }} />}
+                onConfirm={this.handleDelete}>
+                <Button
+                  size="small"
+                  icon={<DeleteOutlined />}
+                  type="primary"
+                  danger
+                  disabled={
+                    !this.context.selectedId ||
+                    isNodeFolder(
+                      this.props.rtModel.elementAt(["tree", "nodes"]),
+                      this.context.selectedId
+                    )
+                  }>
+                  {" "}
+                  Delete{" "}
+                </Button>
+              </Popconfirm>
+            </Col>
+            <Col lg={24} xl={8}>
+              <Button
+                size="small"
+                icon={<CaretRightOutlined />}
+                type="primary"
+                disabled={
+                  !this.context.activeKey ||
+                  isNodeFolder(
+                    this.props.rtModel.elementAt(["tree", "nodes"]),
+                    this.context.selectedId
+                  )
+                }
+                onClick={this.context.runTerminal}>
+                Run{" "}
+              </Button>
+            </Col>
+          </Row>
+        </Col>
 
-                <Col xs={24}>
-                    <DirectoryTree
-                        style={{marginTop: "1rem"}}
-                        multiple
-                        defaultExpandAll
-                        onSelect={this.onSelect}
-                        onExpand={this.onExpand}
-                        treeData={data}
-                    />
-                </Col>
-                <Modal
-                    title="Create File"
-                    visible={this.state.visibleNewFileModal}
-                    onOk={this.handleNewFile}
-                    confirmLoading={this.state.confirmLoading}
-                    onCancel={this.handleCancel}
-                    okButtonProps={{disabled: this.state.newName.length === 0}}>
-                    <Input
-                        value={this.state.newName}
-                        onChange={this.changeNewName}
-                        placeholder="File Name"
-                    />
-                </Modal>
+        <Col xs={24}>
+          <DirectoryTree
+            style={{ marginTop: "1rem" }}
+            multiple
+            defaultExpandAll
+            onSelect={this.onSelect}
+            onExpand={this.onExpand}
+            treeData={data}
+          />
+        </Col>
+        <Modal
+          title="Create File"
+          visible={this.state.visibleNewFileModal}
+          onOk={this.handleNewFile}
+          confirmLoading={this.state.confirmLoading}
+          onCancel={this.handleCancel}
+          okButtonProps={{ disabled: this.state.newName.length === 0 }}>
+          <Input
+            value={this.state.newName}
+            onChange={this.changeNewName}
+            placeholder="File Name"
+          />
+        </Modal>
 
-                <Modal
+        {/* <Modal
                     title="Create Folder"
                     visible={this.state.visibleNewFolderModal}
                     onOk={this.handleNewFolder}
@@ -252,8 +264,8 @@ export default class FileManagerComponent extends React.Component {
                         onChange={this.changeNewName}
                         placeholder="Folder Name"
                     />
-                </Modal>
-            </Row>
-        );
-    }
+                </Modal> */}
+      </Row>
+    );
+  }
 }
